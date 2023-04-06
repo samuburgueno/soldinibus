d17753236571541b3aca4a406a525be5a070b4c0
Samuel Burgueño <samuel.burgueno@tiarg.com.ar>
2023-04-05 23:02:22 -0300

    Agrego callback para abrir los horarios desde TL

---
+++ src/pages/callback/[id].js
@@ -0,0 +1,47 @@
+import { LINE_ID } from "@/consts";
+import { getTemporada } from "@/functions";
+import { prisma } from "@/prisma/client";
+import { useEffect, useRef } from "react";
+
+export default function handler({ data, season }) {
+  const formRef = useRef(null);
+
+  useEffect(() => {
+    formRef.current.submit();
+  }, []);
+
+  return (
+    <>
+      <form
+        ref={formRef}
+        id="formid"
+        method="post"
+        action="http://www.etr.gov.ar/includes/chtupV2/verCuadroHorario.php"
+      >
+        <input type="hidden" name="linea" value={LINE_ID} />
+        <input type="hidden" name="temporada" value={season} />
+        <input type="hidden" name="tipo" value={data.type.replace("_", " ")} />
+        <input type="hidden" name="sentido" value={data.way} />
+      </form>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  const data = await prisma.route.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  const season = await getTemporada();
+
+  delete data.updated;
+
+  return {
+    props: {
+      data: data,
+      season,
+    },
+  };
+}
